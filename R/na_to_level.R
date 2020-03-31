8185e2597ff35528d6c022da2ab087af1178ffe9
Patrick R. Wright <patrick.wright@usb.ch>
2020-03-31 13:17:30 +0200

    add dependency handling assertthat

--- R/na_to_level.R
+++ R/na_to_level.R
@@ -3,11 +3,12 @@
 #' @param x a factor
 #' @param name character - the name of the new level (default: N/A)
 #' @return The same factor with renamed NA
+#' @importFrom assertthat assert_that
 #' @export
 #' @examples
 #' na_to_level(factor(c("a","b", "b", NA))) # Levels: a b N/A
 na_to_level <- function(x, name = "N/A") {
-  assertthat::assert_that(is.factor(x))
+  assert_that(is.factor(x))
   
   if(!(name %in% levels(x))) {
     x <- factor(x, levels = c(levels(x), name))
